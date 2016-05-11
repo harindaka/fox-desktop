49d81e178ffff8e6f6943fa7f7a332a5eec988c5
Hemal Harindaka <harindaka@gmail.com>
2016-05-11 12:53:26 +0530

    added build script

--- main.js
+++ main.js
@@ -10,13 +10,22 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800, 
+    height: 600,
+    minWidth: 800,
+    minHeight: 600,
+    title: "Fox 91.4 Desktop Client",
+    autoHideMenuBar: true
+  })
+  
+  mainWindow.setMenu(null);
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html')
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
