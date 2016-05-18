d6d22263e2939f4931615f19ddaa54676cf0973f
Hemal Athulathmudali <hathulathmudali@virtusa.com>
2016-05-18 18:27:54 +0530

    moved launch scripts to the build-tools folder

--- build-tools/build-win.bat
+++ build-tools/build-win.bat
@@ -10,7 +10,7 @@ call node "%PROJECT_PATH%\build-tools\build-helper-rcedit.js" "%PROJECT_PATH%\bi
 move "%PROJECT_PATH%\bin\fox-desktop-win32-ia32" "%PROJECT_PATH%\bin\fox-desktop-win32-ia32-temp"
 mkdir "%PROJECT_PATH%\bin\fox-desktop-win32-ia32"
 move "%PROJECT_PATH%\bin\fox-desktop-win32-ia32-temp" "%PROJECT_PATH%\bin\fox-desktop-win32-ia32\bin"
-copy /Y "%PROJECT_PATH%\start-fox-desktop.exe" "%PROJECT_PATH%\bin\fox-desktop-win32-ia32\start-fox-desktop.exe"
+copy /Y "%PROJECT_PATH%\build-tools\start-fox-desktop.exe" "%PROJECT_PATH%\bin\fox-desktop-win32-ia32\start-fox-desktop.exe"
 call node "%PROJECT_PATH%\build-tools\build-helper-zip.js" "%PROJECT_PATH%\bin\fox-desktop-win32-ia32" "%PROJECT_PATH%\bin\fox-desktop-win32-ia32.zip"
 
 call "%PROJECT_PATH%\node_modules\.bin\electron-packager" "%PROJECT_PATH%" --platform=win32 --arch=x64 --overwrite --icon="%ICON_PATH%" --out="%PROJECT_PATH%\bin" --version-string.CompanyName="Hemal Harindaka" --version-string.ProductName="Fox 91.4 Desktop Client" --version-string.ProductVersion="%VERSION%" --ignore="./bin" --ignore="./node_modules/electron-packager" --ignore="./node_modules/electron-prebuilt"  --ignore="./build-tools"
@@ -18,5 +18,5 @@ call node "%PROJECT_PATH%\build-tools\build-helper-rcedit.js" "%PROJECT_PATH%\bi
 move "%PROJECT_PATH%\bin\fox-desktop-win32-x64" "%PROJECT_PATH%\bin\fox-desktop-win32-x64-temp"
 mkdir "%PROJECT_PATH%\bin\fox-desktop-win32-x64"
 move "%PROJECT_PATH%\bin\fox-desktop-win32-x64-temp" "%PROJECT_PATH%\bin\fox-desktop-win32-x64\bin"
-copy /Y "%PROJECT_PATH%\start-fox-desktop.exe" "%PROJECT_PATH%\bin\fox-desktop-win32-x64\start-fox-desktop.exe"
+copy /Y "%PROJECT_PATH%\build-tools\start-fox-desktop.exe" "%PROJECT_PATH%\bin\fox-desktop-win32-x64\start-fox-desktop.exe"
 call node "%PROJECT_PATH%\build-tools\build-helper-zip.js" "%PROJECT_PATH%\bin\fox-desktop-win32-x64" "%PROJECT_PATH%\bin\fox-desktop-win32-x64.zip"
