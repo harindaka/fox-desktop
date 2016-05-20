0c15e94d53681c2c2a531eccb50d0b0dab79287f
harindaka <harindaka@gmail.com>
2016-05-20 16:59:58 +0530

    added version number to build archive

--- build-tools/build-win.bat
+++ build-tools/build-win.bat
@@ -11,7 +11,7 @@ move "%PROJECT_PATH%\bin\fox-desktop-win32-ia32" "%PROJECT_PATH%\bin\fox-desktop
 mkdir "%PROJECT_PATH%\bin\fox-desktop-win32-ia32"
 move "%PROJECT_PATH%\bin\fox-desktop-win32-ia32-temp" "%PROJECT_PATH%\bin\fox-desktop-win32-ia32\bin"
 copy /Y "%PROJECT_PATH%\build-tools\start-fox-desktop.exe" "%PROJECT_PATH%\bin\fox-desktop-win32-ia32\start-fox-desktop.exe"
-call node "%PROJECT_PATH%\build-tools\build-helper-zip.js" "%PROJECT_PATH%\bin\fox-desktop-win32-ia32" "%PROJECT_PATH%\bin\fox-desktop-win32-ia32.zip"
+call node "%PROJECT_PATH%\build-tools\build-helper-zip.js" "%PROJECT_PATH%\bin\fox-desktop-win32-ia32" "%PROJECT_PATH%\bin\fox-desktop-v%VERSION%-win32-ia32.zip"
 
 call "%PROJECT_PATH%\node_modules\.bin\electron-packager" "%PROJECT_PATH%" --platform=win32 --arch=x64 --overwrite --icon="%ICON_PATH%" --out="%PROJECT_PATH%\bin" --version-string.CompanyName="Hemal Harindaka" --version-string.ProductName="Fox 91.4 Desktop Client" --version-string.ProductVersion="%VERSION%" --ignore="./bin" --ignore="./node_modules/electron-packager" --ignore="./node_modules/electron-prebuilt"  --ignore="./build-tools"
 call node "%PROJECT_PATH%\build-tools\build-helper-rcedit.js" "%PROJECT_PATH%\bin\fox-desktop-win32-x64\fox-desktop.exe" "version-string=%VERSION%" "version-string=%VERSION%" "file-version=%VERSION%" "product-version=%VERSION%" "icon=%ICON_PATH%"
@@ -19,4 +19,4 @@ move "%PROJECT_PATH%\bin\fox-desktop-win32-x64" "%PROJECT_PATH%\bin\fox-desktop-
 mkdir "%PROJECT_PATH%\bin\fox-desktop-win32-x64"
 move "%PROJECT_PATH%\bin\fox-desktop-win32-x64-temp" "%PROJECT_PATH%\bin\fox-desktop-win32-x64\bin"
 copy /Y "%PROJECT_PATH%\build-tools\start-fox-desktop.exe" "%PROJECT_PATH%\bin\fox-desktop-win32-x64\start-fox-desktop.exe"
-call node "%PROJECT_PATH%\build-tools\build-helper-zip.js" "%PROJECT_PATH%\bin\fox-desktop-win32-x64" "%PROJECT_PATH%\bin\fox-desktop-win32-x64.zip"
+call node "%PROJECT_PATH%\build-tools\build-helper-zip.js" "%PROJECT_PATH%\bin\fox-desktop-win32-x64" "%PROJECT_PATH%\bin\fox-desktop-v%VERSION%-win32-x64.zip"
