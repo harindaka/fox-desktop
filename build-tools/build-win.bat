1d66a0972ef8f0e1d39ccf7a892fb4c2a9025780
Hemal Harindaka <harindaka@gmail.com>
2016-05-21 01:32:04 +0530

    added output path variable to build scripts

--- build-tools/build-win.bat
+++ build-tools/build-win.bat
@@ -4,19 +4,22 @@ cd %~dp0
 
 SET VERSION=1.0
 SET ICON_PATH=%PROJECT_PATH%\images\fox.ico
+SET OUT_PATH=%PROJECT_PATH%\bin
 
-call "%PROJECT_PATH%\node_modules\.bin\electron-packager" "%PROJECT_PATH%" --platform=win32 --arch=ia32 --overwrite --icon="%ICON_PATH%" --out="%PROJECT_PATH%\bin" --version-string.CompanyName="Hemal Harindaka" --version-string.ProductName="Fox 91.4 Desktop Client" --version-string.ProductVersion="%VERSION%" --ignore="./bin" --ignore="./node_modules/electron-packager" --ignore="./node_modules/electron-prebuilt" --ignore="./build-tools" 
-call node "%PROJECT_PATH%\build-tools\build-helper-rcedit.js" "%PROJECT_PATH%\bin\fox-desktop-win32-ia32\fox-desktop.exe" "version-string=%VERSION%" "version-string=%VERSION%" "file-version=%VERSION%" "product-version=%VERSION%" "icon=%ICON_PATH%"
-move "%PROJECT_PATH%\bin\fox-desktop-win32-ia32" "%PROJECT_PATH%\bin\fox-desktop-win32-ia32-temp"
-mkdir "%PROJECT_PATH%\bin\fox-desktop-win32-ia32"
-move "%PROJECT_PATH%\bin\fox-desktop-win32-ia32-temp" "%PROJECT_PATH%\bin\fox-desktop-win32-ia32\bin"
-copy /Y "%PROJECT_PATH%\build-tools\start-fox-desktop.exe" "%PROJECT_PATH%\bin\fox-desktop-win32-ia32\start-fox-desktop.exe"
-call node "%PROJECT_PATH%\build-tools\build-helper-zip.js" "%PROJECT_PATH%\bin\fox-desktop-win32-ia32" "%PROJECT_PATH%\bin\fox-desktop-v%VERSION%-win32-ia32.zip"
+rmdir "%OUT_PATH%" /s /q
 
-call "%PROJECT_PATH%\node_modules\.bin\electron-packager" "%PROJECT_PATH%" --platform=win32 --arch=x64 --overwrite --icon="%ICON_PATH%" --out="%PROJECT_PATH%\bin" --version-string.CompanyName="Hemal Harindaka" --version-string.ProductName="Fox 91.4 Desktop Client" --version-string.ProductVersion="%VERSION%" --ignore="./bin" --ignore="./node_modules/electron-packager" --ignore="./node_modules/electron-prebuilt"  --ignore="./build-tools"
-call node "%PROJECT_PATH%\build-tools\build-helper-rcedit.js" "%PROJECT_PATH%\bin\fox-desktop-win32-x64\fox-desktop.exe" "version-string=%VERSION%" "version-string=%VERSION%" "file-version=%VERSION%" "product-version=%VERSION%" "icon=%ICON_PATH%"
-move "%PROJECT_PATH%\bin\fox-desktop-win32-x64" "%PROJECT_PATH%\bin\fox-desktop-win32-x64-temp"
-mkdir "%PROJECT_PATH%\bin\fox-desktop-win32-x64"
-move "%PROJECT_PATH%\bin\fox-desktop-win32-x64-temp" "%PROJECT_PATH%\bin\fox-desktop-win32-x64\bin"
-copy /Y "%PROJECT_PATH%\build-tools\start-fox-desktop.exe" "%PROJECT_PATH%\bin\fox-desktop-win32-x64\start-fox-desktop.exe"
-call node "%PROJECT_PATH%\build-tools\build-helper-zip.js" "%PROJECT_PATH%\bin\fox-desktop-win32-x64" "%PROJECT_PATH%\bin\fox-desktop-v%VERSION%-win32-x64.zip"
+call "%PROJECT_PATH%\node_modules\.bin\electron-packager" "%PROJECT_PATH%" --platform=win32 --arch=ia32 --overwrite --icon="%ICON_PATH%" --out="%OUT_PATH%" --version-string.CompanyName="Hemal Harindaka" --version-string.ProductName="Fox 91.4 Desktop Client" --version-string.ProductVersion="%VERSION%" --ignore="./bin" --ignore="./node_modules/electron-packager" --ignore="./node_modules/electron-prebuilt" --ignore="./build-tools" 
+call node "%PROJECT_PATH%\build-tools\build-helper-rcedit.js" "%OUT_PATH%\fox-desktop-win32-ia32\fox-desktop.exe" "version-string=%VERSION%" "version-string=%VERSION%" "file-version=%VERSION%" "product-version=%VERSION%" "icon=%ICON_PATH%"
+move "%OUT_PATH%\fox-desktop-win32-ia32" "%OUT_PATH%\fox-desktop-win32-ia32-temp"
+mkdir "%OUT_PATH%\fox-desktop-win32-ia32"
+move "%OUT_PATH%\fox-desktop-win32-ia32-temp" "%OUT_PATH%\fox-desktop-win32-ia32\bin"
+copy /Y "%PROJECT_PATH%\build-tools\start-fox-desktop.exe" "%OUT_PATH%\fox-desktop-win32-ia32\start-fox-desktop.exe"
+call node "%PROJECT_PATH%\build-tools\build-helper-zip.js" "%OUT_PATH%\fox-desktop-win32-ia32" "%OUT_PATH%\fox-desktop-v%VERSION%-win32-ia32.zip"
+
+call "%PROJECT_PATH%\node_modules\.bin\electron-packager" "%PROJECT_PATH%" --platform=win32 --arch=x64 --overwrite --icon="%ICON_PATH%" --out="%OUT_PATH%" --version-string.CompanyName="Hemal Harindaka" --version-string.ProductName="Fox 91.4 Desktop Client" --version-string.ProductVersion="%VERSION%" --ignore="./bin" --ignore="./node_modules/electron-packager" --ignore="./node_modules/electron-prebuilt"  --ignore="./build-tools"
+call node "%PROJECT_PATH%\build-tools\build-helper-rcedit.js" "%OUT_PATH%\fox-desktop-win32-x64\fox-desktop.exe" "version-string=%VERSION%" "version-string=%VERSION%" "file-version=%VERSION%" "product-version=%VERSION%" "icon=%ICON_PATH%"
+move "%OUT_PATH%\fox-desktop-win32-x64" "%OUT_PATH%\fox-desktop-win32-x64-temp"
+mkdir "%OUT_PATH%\fox-desktop-win32-x64"
+move "%OUT_PATH%\fox-desktop-win32-x64-temp" "%OUT_PATH%\fox-desktop-win32-x64\bin"
+copy /Y "%PROJECT_PATH%\build-tools\start-fox-desktop.exe" "%OUT_PATH%\fox-desktop-win32-x64\start-fox-desktop.exe"
+call node "%PROJECT_PATH%\build-tools\build-helper-zip.js" "%OUT_PATH%\fox-desktop-win32-x64" "%OUT_PATH%\fox-desktop-v%VERSION%-win32-x64.zip"
