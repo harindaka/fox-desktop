84ecbff8f1b2ac3eb0de2101c881023947d21e86
Hemal Harindaka <harindaka@gmail.com>
2016-05-21 01:03:50 +0530

    added social integration

--- local-config.js
+++ local-config.js
@@ -1,5 +1,13 @@
 module.exports = {
 	"version": "1.0",
 	"remoteConfigUrl": "https://raw.githubusercontent.com/harindaka/fox-desktop/master/remote-config.js",
-	"fallbackStreamUrl": "http://72.46.136.154:7061/;stream.mp3"
+	"streamUrl": "http://72.46.136.154:7061/;stream.mp3",
+	"authorUrl": "https://www.linkedin.com/in/harindaka",
+    "projectUrl": "https://github.com/harindaka/fox-desktop",
+    "licenseUrl": "https://www.gnu.org/licenses/gpl-3.0.txt",
+    "fbPageUrl": "https://www.facebook.com/foxdesktop",
+    "fbShareUrl": "https://www.facebook.com/sharer/sharer.php?u=https%3A//www.facebook.com/foxdesktop/",
+    "twitterShareUrl": "https://twitter.com/home?status=Stream%20Fox%2091.4%20FM%20from%20your%20desktop%20with%20%23foxdesktop%20https%3A//github.com/harindaka/fox-desktop",
+    "gplusShareUrl": "https://plus.google.com/share?url=https%3A//github.com/harindaka/fox-desktop",
+    "linkedInShareUrl": "https://www.linkedin.com/shareArticle?mini=true&url=https%3A//github.com/harindaka/fox-desktop&title=Fox%2091.4%20FM%20Desktop%20Client&summary=Fox%20Desktop%20is%20an%20unofficial%20cross%20platform%20streaming%20desktop%20client%20for%20Sri%20Lanka's%20Fox%2091.4%20FM%20Radio.&source="
 }
